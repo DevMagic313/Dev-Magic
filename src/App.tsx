232623aae4eee1c481eea3b00dbfe83f54e49360
DevMagic <devmagic313@gmail.com>
2025-02-11 01:36:00 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Resume from './components/Resume';
-import Blog from './components/Blog';
+// import Blog from './components/Blog';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import { useThemeStore } from './store/themeStore';
